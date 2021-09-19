10f7936f93540be897cbe943a9bad92cdab945d6
rajat <rajat.chaple@yahoo.com>
2021-09-19 01:33:42 -0600

    assignment 4 part 1 threading

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+
 /**
  * This structure should be dynamically allocated and passed as
  * an argument to your thread using pthread_create.
@@ -14,7 +15,13 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
- 
+
+    //wait_to_obtain_ms;
+    //wait_to_release_ms;
+    //pthread_t thread_id;
+    int thread_wait_to_obtain_ms;
+    int thread_wait_to_release_ms;
+    pthread_mutex_t *thread_mutex;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
